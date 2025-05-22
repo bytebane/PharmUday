c5bceaaf152a3a8294fc55dd859dd123e17a039f
bBane <81875670+bytebane@users.noreply.github.com>
2025-05-22 22:13:09 +0530

    ignore-lint-in-build

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: 'standalone',
-	// eslint: {
-	// 	ignoreDuringBuilds: true,
-	// },
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 }
 
 export default nextConfig
